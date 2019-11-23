ac9d89e6091fcfd02d950dd70ede0db93a01513b
Giovani Garfias <giovanicode@outlook.com>
2019-11-22 20:07:17 -0800

    MVP COMPLETED

---
+++ starwars/src/components/People.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { TextContainer, NameTextStyle } from './StyleComponents/CardStyles'
+
+
+const People = props => {
+
+
+    return (
+            <TextContainer>
+            <NameTextStyle>{props.eachPeople.name}</NameTextStyle>
+            <NameTextStyle>{props.eachPeople.gender}</NameTextStyle>
+            <NameTextStyle>{props.eachPeople.height}</NameTextStyle>
+            <NameTextStyle>{props.eachPeople.mass}</NameTextStyle>
+            <NameTextStyle>{props.eachPeople.birth_year}</NameTextStyle>
+            </TextContainer>   
+    );
+}
+
+
+export default People;
+
---
+++ starwars/src/components/PeopleContainer.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import ListOfPeople from './ListOfPeople';
+
+const PeopleContainer = () => {
+    const [ people, setPeople ] = useState();
+
+    useEffect(() => {
+        axios({
+            method: 'get',
+            url: 'https://swapi.co/api/people/'
+          })    
+        .then(response => {
+            console.log("This is what we get back from the API:", response.data.results)
+            setPeople(response.data.results)
+            
+        })
+        .catch(error => console.log(error))
+    }, [])
+
+    return (
+        <>
+            <ListOfPeople 
+                people={people}    
+            />
+        </>
+    )
+};
+
+
+
+export default PeopleContainer;
+
