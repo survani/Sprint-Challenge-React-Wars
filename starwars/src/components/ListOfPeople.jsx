74d5d4ca907e393f367293138bbde2c312a479df
Giovani Garfias <46204471+survani@users.noreply.github.com>
2019-12-02 20:35:38 -0800

    Fun edits after Challenge

--- starwars/src/components/ListOfPeople.jsx
+++ starwars/src/components/ListOfPeople.jsx
@@ -3,14 +3,13 @@ import People from "./People";
 
 
 const ListOfPeople = (props) => {
-    console.log('Look at the props of the List of People:', props.localStateVariable, props)
 
-
+// 'method determines whether the passed value is an Array'
     if (!Array.isArray(props.people)) {
         return (
             <section>
                 <p>Loading...</p>
-            </section>
+            </section> 
         )
     }
 
--- starwars/src/components/People.jsx
+++ starwars/src/components/People.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { TextContainer, NameTextStyle } from './StyleComponents/CardStyles'
 
-
 const People = props => {
 
 
